f591d7b3aec985ef0f97dc372f8e057978d961b3
Prashant-2505 <shramaprashant2505@gmail.com>
2023-09-04 13:22:12 +0530

    .

--- server.js
+++ server.js
@@ -1,51 +1,50 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import connectDb from './config/db.js'
-import morgan from 'morgan'
-import authRoute from "./routes/authRoute.js"
-import categoryRoute from "./routes/categoryRoutes.js"
-import productRoute from './routes/productRoute.js'
-import cors from 'cors'
-import path from 'path'
-import {fileUrlToPath} from 'url'
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDb from './config/db.js';
+import morgan from 'morgan';
+import authRoute from './routes/authRoute.js';
+import categoryRoute from './routes/categoryRoutes.js';
+import productRoute from './routes/productRoute.js';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-//config env
-dotenv.config()
-// database config
-connectDb()
+// Config env
+dotenv.config();
+// Database config
+connectDb();
 
+// ES module fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// es module fix
-const __filename = fileUrlToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const app = express();
 
-
-const app = express()
-
-// middleware
-// Increase the JSON request size limit (adjust the limit as needed)
+// Middleware
+app.use(morgan('dev'));
 app.use(express.json({ limit: '20mb' }));
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(cors())
-app.use(express.static(path.join(__dirname,'./client/build')))
+app.use(cors());
 
-// routes
-// auth route
-app.use('/api/v2/auth',authRoute)
-// category route
-app.use('/api/v2/category',categoryRoute)
-// product route
-app.use('/api/v2/product',productRoute)
+// Serve static files from the 'client/build' directory
+app.use(express.static(path.join(__dirname, './client/build')));
 
-app.use('*',function(req,res)
-{
-    res.sendFile(path.join(__dirname,'./client/build/index.html'))
-})
+// Routes
+app.use('/api/v2/auth', authRoute);
+app.use('/api/v2/category', categoryRoute);
+app.use('/api/v2/product', productRoute);
 
+// Wildcard route for SPA client-side routing
+app.use('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
-const port = process.env.PORT || 8080
-app.listen(port, ()=>
-{
-    console.log(`server is running at ${port}`.bgYellow)
-})
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server is running at ${port}`.bgYellow);
+});
