dcdec9c402e9ce684649ab178c471b1d821a30ba
Prashant-2505 <shramaprashant2505@gmail.com>
2023-09-04 23:56:10 +0530

    fix

--- server.js
+++ server.js
@@ -24,13 +24,11 @@ const app = express();
 // Middleware
 app.use(morgan('dev'));
 app.use(express.json({ limit: '20mb' }));
-app.use(cors());
 
-
-// Serve static files from the 'client/build' directory
+// Define staticPath before using it
 const staticPath = path.join(__dirname, './client/build');
+app.use(cors());
 app.use(express.static(staticPath));
-
 
 // Routes
 app.use('/api/v2/auth', authRoute);
@@ -41,8 +39,6 @@ app.use('/api/v2/product', productRoute);
 app.use('*', (req, res)=>{
   res.sendFile(path.join(__dirname,'./client/build/index.html'));
 });
-
-
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
