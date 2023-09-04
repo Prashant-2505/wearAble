738a69b56a1095275b57e25977118c494635ea6d
Prashant-2505 <shramaprashant2505@gmail.com>
2023-09-04 23:14:14 +0530

    ..

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use('/api/v2/product', productRoute);
 
 // Wildcard route for SPA client-side routing
 app.use('*', (req, res)=>{
-  res.sendFile(path.join(__dirname,'../client/build/index.html'));
+  res.sendFile(path.join(__dirname,'./client/build/index.html'));
 });
 
 
