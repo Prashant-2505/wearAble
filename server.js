5c5e6045c829dce815aff98d754c7c7dcd619f2a
Prashant-2505 <shramaprashant2505@gmail.com>
2023-09-30 19:17:26 +0530

    cors

--- server.js
+++ server.js
@@ -27,7 +27,13 @@ app.use(express.json({ limit: '20mb' }));
 
 // Define staticPath before using it
 const staticPath = path.join(__dirname, 'client','build');
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://clever-cyan-garb.cyclic.app/"],
+    methods:["POST","PUT","GET","DELETE"],
+    credentials:true
+  }
+));
 app.use(express.static(staticPath));
 
 // Routes
