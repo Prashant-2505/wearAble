862dc972b2c5297ed841ca7e1631256620cea803
Prashant-2505 <shramaprashant2505@gmail.com>
2023-10-05 15:49:52 +0530

    ...

--- client/src/components/ProductCrousel/ProductCrousel.jsx
+++ client/src/components/ProductCrousel/ProductCrousel.jsx
@@ -9,15 +9,25 @@ const ProductCrousel = () => {
     const [products, setProducts] = useState([]);
     const [index, setIndex] = useState(0);
     const [nextIndex, setNextIndex] = useState(6);
-    const [length, setLength] = useState(0)
+    const [length, setLength] = useState(0);
+    const [maxItems, setMaxItems] = useState(6);  // Initial number of items to display
 
+    const handleResize = () => {
+        // Adjust the number of items to display based on screen width
+        if (window.innerWidth <= 576) {
+            setMaxItems(3);  // Change this number based on your requirements
+        } else {
+            setMaxItems(6);  // Change this number based on your requirements
+        }
+    };
+    
 
     const allProducts = async () => {
         try {
             const res = await axios.get(`/api/v2/product/get-product/limited`);
             if (res?.data) {
                 setProducts(res?.data?.product);
-                setLength(res?.data?.total)
+                setLength(res?.data?.total);
             }
         } catch (error) {
             console.log(error);
@@ -26,21 +36,23 @@ const ProductCrousel = () => {
 
     useEffect(() => {
         allProducts();
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
     }, []);
-
-
 
     const previous = () => {
         if (index !== 0) {
-            setIndex(index - 6);
-            setNextIndex(nextIndex - 6);
+            setIndex(index - maxItems);
+            setNextIndex(nextIndex - maxItems);
         }
     };
 
     const next = () => {
-        if (index < length - 6) {
+        if (index < length - maxItems) {
             setIndex(nextIndex);
-            setNextIndex(nextIndex + 6);
+            setNextIndex(nextIndex + maxItems);
         }
     };
 
@@ -48,12 +60,9 @@ const ProductCrousel = () => {
         <div className='craousel-component'>
             <h1>Fall in comfort</h1>
             <p>Our top picks for a consciously cozy season.</p>
-            <div
-                className='crousel-product-card'
-
-            >
+            <div className='crousel-product-card'>
                 {products.slice(index, nextIndex).map((p) => (
-                    <Link className='link' to={`/product/${p.slug}`} key={p._id }>
+                    <Link className='link' to={`/product/${p.slug}`} key={p._id}>
                         <ProductCard
                             name={p.name}
                             price={p.price}
